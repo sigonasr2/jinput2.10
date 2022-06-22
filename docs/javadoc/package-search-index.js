490bb5a69cba488169135911b257d2e353257833
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-06-22 19:13:50 +0000

    Correct the location of the forked project in javadocs

--- docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.java.games.input"},{"l":"net.java.games.util"},{"l":"net.java.games.util.plugins"}];updateSearchResults();
